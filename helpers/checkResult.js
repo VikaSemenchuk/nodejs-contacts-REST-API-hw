bfb90f222edff144b25c2064664028ab7c03053e
Vika Semenchuk <vikasemenchuk@MacBook-Air-Vika.local>
2023-06-21 01:58:49 +0300

    ADDES

---
+++ helpers/checkResult.js
@@ -0,0 +1,9 @@
+const HttpError  = require("./HttpError");
+
+const checkResult = result => {
+    if (!result) {
+        throw HttpError(404, "Not found");
+      }
+}
+
+module.exports = checkResult;
---
+++ helpers/index.js
@@ -0,0 +1,10 @@
+const HttpError = require('./HttpError');
+const ctrlWrapper = require('./ctrlWrapper')
+const checkResult = require('./checkResult')
+
+module.exports = {
+    HttpError,
+    ctrlWrapper,
+    checkResult
+
+}
