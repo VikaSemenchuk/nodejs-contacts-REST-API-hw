05c15a83013a2bf32f838e87ef69b8eeeca3eef8
Vika Semenchuk <vikasemenchuk@MacBook-Air-Vika.local>
2023-06-21 21:05:51 +0300

    FINISHED: hw03

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,11 @@
-const HttpError = require('./HttpError');
-const ctrlWrapper = require('./ctrlWrapper')
-const checkResult = require('./checkResult')
+const HttpError = require("./HttpError");
+const ctrlWrapper = require("./ctrlWrapper");
+const checkResult = require("./checkResult");
+const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
-    HttpError,
-    ctrlWrapper,
-    checkResult
-
-}
+  HttpError,
+  ctrlWrapper,
+  checkResult,
+  handleMongooseError,
+};
--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const mongoose = require('mongoose');
-const app = require('./app')
-// const DB_HOST = require('./config')
-const {DB_HOST, PORT} = process.env
-// 
+const mongoose = require("mongoose");
+const app = require("./app");
+const { DB_HOST, PORT } = process.env;
 
-mongoose.connect(DB_HOST)
-.then(() => {
-  console.log("Database connection successful");
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
 
-  app.listen(PORT, () => {
-    console.log("Server running. Use our API on port: 3000")
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
   });
-})
-.catch(error => {
-  console.log(error.message);
-  process.exit(1);
-})
-
-
-
-
-
